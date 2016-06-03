d872f7fd4d5112376f52ed532ad2f2af0e267cb8
Akash Desai <kash650@gmail.com>
2016-06-03 16:17:47 -0700

    added FAT library, bitcode enabled inside

--- DopamineKit.podspec
+++ DopamineKit.podspec
@@ -8,14 +8,8 @@
 
 Pod::Spec.new do |s|
   s.name             = "DopamineKit"
-  s.version          = "0.2.0"
+  s.version          = "3.0.0"
   s.summary          = "A framework to use DopamineLabs machine learning API"
-
-# This description is used to generate tags and improve search results.
-#   * Think: What does it do? Why did you write it? What is the focus?
-#   * Try to keep it short, snappy and to the point.
-#   * Write the description between the DESC delimiters below.
-#   * Finally, don't worry about the indent, CocoaPods strips it!
 
   s.description      = <<-DESC
 Make your iOS app habit-forming using the Dopamine API.
@@ -29,8 +23,6 @@ This packages provides a framework for interacting with the Dopamine API from a 
   s.source           = { :git => "https://github.com/DopamineLabs/DopamineAPI_Swift-CocoaPod.git", :tag => s.version.to_s }
 
   s.ios.deployment_target = '8.0'
-
-# s.source_files = 'DopamineKit/Classes/**/*'
   s.ios.vendored_frameworks = 'DopamineKit/Frameworks/DopamineKit.framework'
 
 end
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -6,7 +6,10 @@ class Tests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // Set the plist so DopamineKit can read the AppID, API keys, ...
+        let path = NSBundle(forClass: self.dynamicType).pathForResource("DopamineProperties", ofType: "plist")
+        DopamineKit.instance.plistPath = path as String!
     }
     
     override func tearDown() {
@@ -14,16 +17,35 @@ class Tests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    // Test DopamineKit.reinforce()
+    func testReinforceRequest() {
+        NSLog("alk;sdjfal;skdfj")
+        let asyncExpectation = expectationWithDescription("Reinforcement decision")
+        DopamineKit.reinforce("action1", callback: {response in
+            NSLog("DopamineKitTest reinforce resulted in:\(response)")
+            asyncExpectation.fulfill()
+        })
+        
+        waitForExpectationsWithTimeout(10, handler: {error in
+            XCTAssertNil(error, "DopamineKitTest error: reinforce request timed out")
+        })
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
+    // Test DopamineKit.track()
+    func testTrackingRequest() {
+        NSLog("alk;sdjfal;skdfj")
+//        DopamineKit.tra
+        let asyncExpectation = expectationWithDescription("Tracking request")
+        DopamineKit.track(actionID: "test", callback: {response in
+            NSLog("DopamineKitTest tracking resulted in:\(response)")
+            asyncExpectation.fulfill()
+        })
+        
+        
+        waitForExpectationsWithTimeout(10, handler: {error in
+            XCTAssertNil(error, "DopamineKitTest error: track request timed out")
+        })
+        
     }
     
 }
