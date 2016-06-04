bfc5b6bdb1135cbd7253bcee2bd77d901006fe22
Akash Desai <kash650@gmail.com>
2016-06-04 00:26:36 -0700

    fixed test unit

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -4,22 +4,18 @@ import DopamineKit
 
 class Tests: XCTestCase {
     
+    /// Note: The NSBundle.mainBundle() must have DopamineProperites.plist in its path
     override func setUp() {
         super.setUp()
-        
-        // Set the plist so DopamineKit can read the AppID, API keys, ...
-        let path = NSBundle(forClass: self.dynamicType).pathForResource("DopamineProperties", ofType: "plist")
-        DopamineKit.instance.plistPath = path as String!
+        // read the note
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    // Test DopamineKit.reinforce()
+    /// Test DopamineKit.reinforce(actionID,callback)
     func testReinforceRequest() {
-        NSLog("alk;sdjfal;skdfj")
         let asyncExpectation = expectationWithDescription("Reinforcement decision")
         DopamineKit.reinforce("action1", callback: {response in
             NSLog("DopamineKitTest reinforce resulted in:\(response)")
@@ -31,21 +27,9 @@ class Tests: XCTestCase {
         })
     }
     
-    // Test DopamineKit.track()
+    /// Test DopamineKit.track(actionID)
     func testTrackingRequest() {
-        NSLog("alk;sdjfal;skdfj")
-//        DopamineKit.tra
-        let asyncExpectation = expectationWithDescription("Tracking request")
-        DopamineKit.track(actionID: "test", callback: {response in
-            NSLog("DopamineKitTest tracking resulted in:\(response)")
-            asyncExpectation.fulfill()
-        })
-        
-        
-        waitForExpectationsWithTimeout(10, handler: {error in
-            XCTAssertNil(error, "DopamineKitTest error: track request timed out")
-        })
-        
+        DopamineKit.track("test")
     }
     
 }
