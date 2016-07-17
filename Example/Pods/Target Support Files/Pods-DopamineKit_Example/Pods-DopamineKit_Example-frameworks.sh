4f5d71329dcbeede37df11f7ac1d51f15de40d4a
Akash Desai <kash650@gmail.com>
2016-07-17 15:30:52 -0700

    changing config dir location

--- Example/Pods/Target Support Files/Pods-DopamineKit_Example/Pods-DopamineKit_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-DopamineKit_Example/Pods-DopamineKit_Example-frameworks.sh
@@ -84,8 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/DopamineKit/DopamineKit.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/DopamineKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/DopamineKit/DopamineKit.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/DopamineKit.framework"
 fi
