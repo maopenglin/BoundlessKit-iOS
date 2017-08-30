1c7e973a4b088e4070dd56c44e57ec6775a71951
Akash Desai <kash650@gmail.com>
2017-08-29 18:56:09 -0700

    capturing pinching and long presses

--- Sources/Categories/UIWindow+Dopamine.m
+++ Sources/Categories/UIWindow+Dopamine.m
@@ -19,161 +19,103 @@
     [SwizzleHelper injectSelector:[DopamineWindow class] :@selector(swizzled_sendEvent:) :[UIWindow class] :@selector(sendEvent:)];
 }
 
-static bool singleGesture = false;
-static UIView* gestureView;
-
+static NSUInteger firstTouchHash;
 static uint64_t startTouchTime;
 static CGPoint startTouchPosition1;
 static CGPoint startTouchPosition2;
-static CGFloat ZOOM_DRAG_MIN = 30;
-static CGFloat SWIPE_DRAG_MIN = 15;
+static CGFloat SWIPE_DRAG_MIN = 80;
+static CGFloat PINCH_MIN = 20;
 
 -(void) swizzled_sendEvent: (UIEvent *) event {
-//    for (UITouch *touch in event.allTouches){
-//        if (touch.phase == UITouchPhaseBegan){
-//            // TO-DO: ensure view responds to the event, otherwise get superviews
-//            [EventLogger logEventWithTouch:touch gestureName:@"tap"];
-//        } else if (touch.phase == UITouchPhaseEnded){
-////            [EventLogger logEventWithType:[EventLogger EVENT_TYPE_TOUCHED] forObject:touch.view];
-////            [Helper setLastTouch:touch];
-//            [Helper sendTouchWithTouch:touch];
-//            [Helper sendEventWithEvent:event];
-//            
-//        }
-//    }
     
-    /*
-     * Test code
-     */
-    
-    NSSet<UITouch*>* allTouches = [event allTouches];
-    UITouch* touch = [allTouches anyObject];
-    UIView* touchView = [touch view];
-    
-    for (UITouch* touch in event.allTouches) {
+    if (event.allTouches.count == 1) {
+        UITouch *touch = event.allTouches.anyObject;
         
-        if (event.allTouches.count == 1) {
+        if (touch.phase == UITouchPhaseBegan) {
+            // could be swipe or tap or hold
+            firstTouchHash = touch.hash;
             
-            if (touch.phase == UITouchPhaseBegan) {
-                    // could be swipe or tap or hold
-                    singleGesture = true;
-                    gestureView = [touch view];
-                    startTouchTime = [touch timestamp];
-                    startTouchPosition1 = [touch locationInView:self];
-            }
-            else if (touch.phase == UITouchPhaseMoved) {
-                    // swipe or drag. determine at phaseEnded
-            }
-            else if (touch.phase == UITouchPhaseEnded) {
-                // tap or hold or swipe or drag
+            startTouchTime = [touch timestamp];
+            startTouchPosition1 = [touch locationInView:self];
+        } else if (touch.phase == UITouchPhaseMoved) {
+            // swipe or drag. determine at phaseEnded
+        } else if (touch.phase == UITouchPhaseEnded && touch.hash == firstTouchHash) {
+            // tap or hold or swipe or drag
+            
+            CGPoint currentTouchPosition = [touch locationInView:self];
+            double xDistance = fabs(startTouchPosition1.x - currentTouchPosition.x);
+            double yDistance = fabs(startTouchPosition1.y - currentTouchPosition.y);
+            
+            if (xDistance >= SWIPE_DRAG_MIN || yDistance >= SWIPE_DRAG_MIN) {
                 
-                CGPoint currentTouchPosition = [touch locationInView:self];
-                double xDistance = fabs(startTouchPosition1.x - currentTouchPosition.x);
-                double yDistance = fabs(startTouchPosition1.y - currentTouchPosition.y);
-                
-                if (xDistance >= SWIPE_DRAG_MIN || yDistance >= SWIPE_DRAG_MIN) {
-                    
-                    NSString* swipeOrDrag = (touch.timestamp - startTouchTime < 1.0) ? @"Swipe" : @"Drag";
-                        if (xDistance > yDistance) {
-                            if (currentTouchPosition.x > startTouchPosition1.x) {
-                                NSLog(@"%@ right", swipeOrDrag);
-                            } else {
-                                NSLog(@"%@ left", swipeOrDrag);
-                            }
-                        } else {
-                            if (currentTouchPosition.y > startTouchPosition1.y) {
-                                NSLog(@"%@ down", swipeOrDrag);
-                            } else {
-                                NSLog(@"%@ up", swipeOrDrag);
-                            }
-                        }
-                    
-                } else if (touch.tapCount > 0) {
-                    
-                    NSLog(@"tapCount:(%lu)", (unsigned long)touch.tapCount);
-                    
+                NSString* swipeOrDrag = (touch.timestamp - startTouchTime < 1.0) ? @"Swipe" : @"Drag";
+                if (xDistance > yDistance) {
+                    if (currentTouchPosition.x > startTouchPosition1.x) {
+                        NSLog(@"%@ right", swipeOrDrag);
+                    } else {
+                        NSLog(@"%@ left", swipeOrDrag);
+                    }
+                } else {
+                    if (currentTouchPosition.y > startTouchPosition1.y) {
+                        NSLog(@"%@ down", swipeOrDrag);
+                    } else {
+                        NSLog(@"%@ up", swipeOrDrag);
+                    }
                 }
                 
-                singleGesture = false;
+            } else if (touch.tapCount > 0) {
+                NSLog(@"tapCount:(%lu)", (unsigned long)touch.tapCount);
+            } else {
+                NSLog(@"Holding touch for %fs", touch.timestamp - startTouchTime);
             }
-            else if (touch.phase == UITouchPhaseCancelled) {
-                singleGesture = false;
-            }
-            
-        } else if (event.allTouches.count == 2) {
-//        
-//            if (touch.phase == UITouchPhaseBegan) {
-//                startTouchPosition1 = [touch locationInView:self];
-//                startTouchTime = touch.timestamp;
-//                
-//                if ([allTouches count] > 1) {
-//                    startTouchPosition2 = [[[allTouches allObjects] objectAtIndex:1] locationInView:self];
-//                    previousTouchPosition1 = startTouchPosition1;
-//                    previousTouchPosition2 = startTouchPosition2;
-//                }
-//            }
-//            
-//            else if (touch.phase == UITouchPhaseMoved) {
-//                
-//                if ([allTouches count] > 1) {
-//                    CGPoint currentTouchPosition1 = [[[allTouches allObjects] objectAtIndex:0] locationInView:self];
-//                    CGPoint currentTouchPosition2 = [[[allTouches allObjects] objectAtIndex:1] locationInView:self];
-//                    
-//                    CGFloat currentFingerDistance = hypot((currentTouchPosition1.x - currentTouchPosition2.x), (currentTouchPosition1.y - currentTouchPosition2.y));
-//                    CGFloat previousFingerDistance = hypot((previousTouchPosition1.x - previousTouchPosition2.x), (previousTouchPosition1.y - previousTouchPosition2.y));
-//                    if (fabs(currentFingerDistance - previousFingerDistance) > ZOOM_DRAG_MIN) {
-//                        //                NSNumber* movedDistance = [NSNumber numberWithFloat:currentFingerDistance - previousFingerDistance];
-//                        if (currentFingerDistance > previousFingerDistance) {
-//                            //                          NSLog(@"zoom in");
-//                            [EventLogger logEventWithTouch:touch gestureName:@"zoomIn"];
-//                        } else {
-//                            //                          NSLog(@"zoom out");
-//                            [EventLogger logEventWithTouch:touch gestureName:@"zoomOut"];
-//                        }
-//                    }
-//                }
-//            }
-//            
-//            else if (touch.phase == UITouchPhaseEnded) {
-//                CGPoint currentTouchPosition = [touch locationInView:self];
-//                
-//                // Check if it's a swipe
-//                if (fabs(startTouchPosition1.x - currentTouchPosition.x) >= SWIPE_DRAG_HORIZ_MIN &&
-//                    fabs(startTouchPosition1.x - currentTouchPosition.x) > fabs(startTouchPosition1.y - currentTouchPosition.y) &&
-//                    touch.timestamp - startTouchTime < 0.7)
-//                {
-//                    // It appears to be a swipe.
-//                    if (startTouchPosition1.x < currentTouchPosition.x) {
-//                        //                NSLog(@"swipe right");
-//                        [EventLogger logEventWithTouch:touch gestureName:@"swipeRight"];
-//                    } else {
-//                        //                NSLog(@"swipe left");
-//                        [EventLogger logEventWithTouch:touch gestureName:@"swipeLeft"];
-//                    }
-//                } else {
-//                    //-- else, check if it's a single touch
-//                    if (touch.tapCount == 1) {
-//                        [EventLogger logEventWithTouch:touch gestureName:@"tap"];
-//                    }/* else if (touch.tapCount > 1) {
-//                      handle multi-touch
-//                      }
-//                      */
-//                }
-//                
-//                startTouchPosition1 = CGPointMake(-1, -1);
-//                _initialView = nil;
-//            }
-//            
-//            else if (touch.phase == UITouchPhaseCancelled) {
-//                _initialView = nil;
-//                //          NSLog(@"TOUCH CANCEL");
-//            }
+            firstTouchHash = false;
         }
+        else if (touch.phase == UITouchPhaseCancelled) {
+            firstTouchHash = false;
+        }
+        
+    } else if (event.allTouches.count == 2) {
+        
+        UITouch *touch1;
+        UITouch *touch2;
+        if (event.allTouches.allObjects[0].hash == firstTouchHash) {
+            touch1 = event.allTouches.allObjects[0];
+            touch2 = event.allTouches.allObjects[1];
+        } else {
+            touch1 = event.allTouches.allObjects[1];
+            touch2 = event.allTouches.allObjects[0];
+        }
+        
+        if (touch2.view == NULL) {
+            // touch2 in same view as touch1
+            if (touch2.phase == UITouchPhaseBegan) {
+                startTouchPosition2 = [touch2 locationInView:self];
+            } else if (touch1.phase == UITouchPhaseEnded || touch2.phase == UITouchPhaseEnded) {
+                CGPoint currentTouchPosition1 = [touch1 locationInView:self];
+                CGPoint currentTouchPosition2 = [touch2 locationInView:self];
+                
+                CGFloat startFingerDistance = hypot((startTouchPosition1.x-startTouchPosition2.x), (startTouchPosition1.y-startTouchPosition2.y));
+                CGFloat currentFingerDistance = hypot((currentTouchPosition1.x-currentTouchPosition2.x), (currentTouchPosition1.y-currentTouchPosition2.y));
+                CGFloat pinchDifference = currentFingerDistance - startFingerDistance;
+                if (fabs(pinchDifference) > PINCH_MIN) {
+                    if (currentFingerDistance > startFingerDistance) {
+                        NSLog(@"Zoom in");
+                    } else {
+                        NSLog(@"Zoom out");
+                    }
+                }
+                
+                if (touch2.phase == UITouchPhaseEnded) {
+                    startTouchTime = [touch1 timestamp];
+                    startTouchPosition1 = [touch1 locationInView:self];
+                }
+            }
+        } else {
+            // Will support complicated multi-touch later
+        }
+    } else {
+        // Will support multi-touch later
     }
-    
-    /*
-     * /Test Code
-     */
     
     [self swizzled_sendEvent:event];
 }
