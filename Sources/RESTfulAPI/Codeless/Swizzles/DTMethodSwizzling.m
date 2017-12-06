7631a29bde19fcfb5c40e8dcfb40b5f25606d956
Akash Desai <kash650@gmail.com>
2017-12-06 15:29:44 -0800

    testing UIView swizzle

--- Sources/RESTfulAPI/Codeless/Swizzles/DTMethodSwizzling.m
+++ Sources/RESTfulAPI/Codeless/Swizzles/DTMethodSwizzling.m
@@ -15,6 +15,7 @@
 #import <UIApplicationDelegate+Dopamine.h>
 #import <UIApplication+Dopamine.h>
 #import <UIViewController+Dopamine.h>
+#import <UIView+Dopamine.h>
 #import <SKPaymentTransactionObserver+Dopamine.h>
 
 @implementation UIApplication (Dopamine)
@@ -31,6 +32,9 @@
         
         // Swizzle - UIViewController
         [DopamineViewController swizzleSelectors];
+        
+        // Swizzle - UIView
+        [DopamineView swizzleSelectors];
         
         // Swizzle - SKPaymentTransactionObserver
         [DopaminePaymentTransactionObserver swizzleSelectors];
---
+++ Sources/RESTfulAPI/Codeless/Swizzles/UIView+Dopamine.h
@@ -0,0 +1,18 @@
+//
+//  UIView+Dopamine.h
+//  Pods
+//
+//  Created by Akash Desai on 12/6/17.
+//
+//
+
+#ifndef UIView_Dopamine_h
+#define UIView_Dopamine_h
+
+@interface DopamineView : UIView
+
++ (void) swizzleSelectors;
+
+@end
+
+#endif /* UIView_Dopamine_h */
