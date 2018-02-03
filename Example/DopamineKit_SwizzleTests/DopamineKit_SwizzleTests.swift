afae79127df24536301638c65e4f6df86f1514b1
Akash Desai <kash650@gmail.com>
2018-02-02 18:41:49 -0800

    function to test swizzle success

--- Example/DopamineKit_SwizzleTests/DopamineKit_SwizzleTests.swift
+++ Example/DopamineKit_SwizzleTests/DopamineKit_SwizzleTests.swift
@@ -10,33 +10,6 @@ import XCTest
 @testable import DopamineKit
 @testable import DopamineKit_Example
 
-
-extension ViewController {
-    @objc func testFunc() {
-        let count = Thread.callStackSymbols.count
-        DopamineKit_SwizzleTests.counter = count
-        print("In \(#function) count:\(count)")
-    }
-    
-    @objc func testFunc2() {
-        let count = Thread.callStackSymbols.count
-        DopamineKit_SwizzleTests.counter = count
-        print("In \(#function) count:\(count)")
-    }
-    
-    @objc func testFuncWithObject(str: Int32) {
-        let count = Thread.callStackSymbols.count
-        DopamineKit_SwizzleTests.counter = count
-        print("In \(#function) with object:\(str) count:\(count)")
-    }
-    
-    @objc func testFuncWithObject2(str: String) {
-        let count = Thread.callStackSymbols.count
-        DopamineKit_SwizzleTests.counter = count
-        print("In \(#function) with object:\(str) count:\(count)")
-    }
-}
-
 class DopamineKit_SwizzleTests: XCTestCase {
     
     static var counter = 0
@@ -46,70 +19,76 @@ class DopamineKit_SwizzleTests: XCTestCase {
         super.setUp()
         DopamineVersion.current.update(visualizer: nil)
         
-        
         controllerUnderTest = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() as! ViewController!
         
     }
     
     override func tearDown() {
-//        controllerUnderTest = nil
         super.tearDown()
     }
     
-    func testSimpleMethodSwizzle() {
-        
-        //given
+    func testSwizzleForMethodReturnsVoidParam0() {
         var controllerUnderTest: ViewController!
         controllerUnderTest = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() as! ViewController!
-        XCTAssert(DopamineConfiguration.current.integrationMethod == "codeless")
-        controllerUnderTest.testFunc()
-        let beforeTestFuncStackSize = DopamineKit_SwizzleTests.counter
-        controllerUnderTest.testFunc2()
-        let beforeTestFunc2StackSize = DopamineKit_SwizzleTests.counter
         
-        
-        // when
-        SelectorReinforcement.registerSimpleMethod(classType: ViewController.self, selector: #selector(ViewController.testFunc), reinforcement: ["test": ["Hello!"]])
-        SelectorReinforcement.registerSimpleMethod(classType: ViewController.self, selector: #selector(ViewController.testFunc2), reinforcement: ["test": ["Hello!"]])
-        
-        
-        // then
-        XCTAssert(SelectorReinforcement.isRegistered(classType: ViewController.self, selector: #selector(ViewController.testFunc)), "Selector was not registered")
-        XCTAssert(SelectorReinforcement.isRegistered(classType: ViewController.self, selector: #selector(ViewController.testFunc2)), "Selector was not registered")
-        
-        controllerUnderTest.testFunc()
-        let afterTestFuncStackSize = DopamineKit_SwizzleTests.counter
-        controllerUnderTest.testFunc2()
-        let afterTestFunc2StackSize = DopamineKit_SwizzleTests.counter
-        
-        XCTAssert(afterTestFuncStackSize == beforeTestFuncStackSize + 1, "Swizzle did not happen")
-        XCTAssert(afterTestFunc2StackSize == beforeTestFunc2StackSize + 1, "Swizzle did not happen")
+        doubleSwizzle(
+            sut: controllerUnderTest,
+            selector1: #selector(ViewController.funcReturnVoidParams0),
+            args1: [],
+            selector2: #selector(ViewController.func2ReturnVoidParams0),
+            args2: [],
+            argsCount: 0
+        )
     }
     
-    func testSimpleMethodWithObjectsSwizzle() {
-        
-        //given
+    func testSwizzleForMethodReturnsVoidParam1obj() {
         var controllerUnderTest: ViewController!
         controllerUnderTest = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() as! ViewController!
+        
+        doubleSwizzle(
+            sut: controllerUnderTest,
+            selector1: #selector(ViewController.funcReturnsVoidParam1obj(param1:)),
+            args1: ["blue"],
+            selector2: #selector(ViewController.func2ReturnsVoidParam1obj(param1:)),
+            args2: ["red"],
+            argsCount: 1
+        )
+    }
+    
+    func testSwizzleForMethodReturnsVoidParam1int() {
+        var controllerUnderTest: ViewController!
+        controllerUnderTest = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() as! ViewController!
+        
+        doubleSwizzle(
+            sut: controllerUnderTest,
+            selector1: #selector(ViewController.funcReturnsVoidParam1int(param1:)),
+            args1: [10],
+            selector2: #selector(ViewController.func2ReturnsVoidParam1int(param1:)),
+            args2: [20],
+            argsCount: 1
+        )
+    }
+    
+    func doubleSwizzle(sut: NSObject, selector1: Selector, args1: [Any], selector2: Selector, args2: [Any], argsCount: Int) {
+        
+        // given
         XCTAssert(DopamineConfiguration.current.integrationMethod == "codeless")
-        controllerUnderTest.testFuncWithObject(str: 2)
+        sut.performVariableSelector(sel: selector1, argsCount: argsCount, args: args1)
         let beforeTestFuncStackSize = DopamineKit_SwizzleTests.counter
-        controllerUnderTest.testFuncWithObject2(str: "goodbye")
+        sut.performVariableSelector(sel: selector2, argsCount: argsCount, args: args2)
         let beforeTestFunc2StackSize = DopamineKit_SwizzleTests.counter
         
-        
         // when
-        SelectorReinforcement.registerSimpleMethod(classType: ViewController.self, selector: #selector(ViewController.testFuncWithObject(str:)), reinforcement: ["test": ["Hello!"]])
-        SelectorReinforcement.registerSimpleMethod(classType: ViewController.self, selector: #selector(ViewController.testFuncWithObject2(str:)), reinforcement: ["test": ["Hello!"]])
+        SelectorReinforcement.registerSimpleMethod(classType: type(of: sut), selector: selector1, reinforcement: ["reward": ["rewardForFirst": ["Hello!"]]])
+        SelectorReinforcement.registerSimpleMethod(classType: type(of: sut), selector: selector2, reinforcement: ["reward": ["rewardForSecond": ["Hello!"]]])
+        XCTAssert(SelectorReinforcement.isRegistered(classType: type(of: sut), selector: selector1), "Selector was not registered")
+        XCTAssert(SelectorReinforcement.isRegistered(classType: type(of: sut), selector: selector2), "Selector was not registered")
         
         
         // then
-        XCTAssert(SelectorReinforcement.isRegistered(classType: ViewController.self, selector: #selector(ViewController.testFuncWithObject(str:))), "Selector was not registered")
-        XCTAssert(SelectorReinforcement.isRegistered(classType: ViewController.self, selector: #selector(ViewController.testFuncWithObject2(str:))), "Selector was not registered")
-        
-        controllerUnderTest.testFuncWithObject(str: 2)
+        sut.performVariableSelector(sel: selector1, argsCount: argsCount, args: args1)
         let afterTestFuncStackSize = DopamineKit_SwizzleTests.counter
-        controllerUnderTest.testFuncWithObject2(str: "goodbye")
+        sut.performVariableSelector(sel: selector2, argsCount: argsCount, args: args2)
         let afterTestFunc2StackSize = DopamineKit_SwizzleTests.counter
         
         XCTAssert(afterTestFuncStackSize == beforeTestFuncStackSize + 1, "Swizzle did not happen")
@@ -152,4 +131,14 @@ class DopamineKit_SwizzleTests: XCTestCase {
     
 }
 
-
+extension NSObject {
+    func performVariableSelector(sel: Selector, argsCount: Int, args: [Any]) {
+        if (argsCount == 0) {
+            self.perform(sel)
+        } else if (argsCount == 1) {
+            self.perform(sel, with: args[0])
+        } else {
+            XCTAssert(false, "Too many args")
+        }
+    }
+}
