f746590b033346ca8eb0106b3d9b29296c99ad56
Akash Desai <kash650@gmail.com>
2018-02-06 23:27:46 -0800

    fixed method encodings byte offset bug

--- Sources/RESTfulAPI/Codeless/Swizzles/NSObject+Dopamine.m
+++ Sources/RESTfulAPI/Codeless/Swizzles/NSObject+Dopamine.m
@@ -13,23 +13,23 @@
 @implementation DopamineObject
 
 + (SEL) createReinforcedMethodFor :(Class) targetClass :(SEL) originalSelector :(SEL) newSelector {
-    Method originalMethod = class_getInstanceMethod(targetClass, originalSelector);
-    if (originalMethod == nil) {
-        return nil;
-    }
     if (class_getInstanceMethod(targetClass, newSelector)) {
         return newSelector;
     }
     
+    Method originalMethod = class_getInstanceMethod(targetClass, originalSelector);
+    if (originalMethod == nil) {
+        return nil;
+    }
     const char* methodTypeEncoding = method_getTypeEncoding(originalMethod);
-    NSString* methodTypeEncodingString = @(methodTypeEncoding);
+    NSString* methodTypeEncodingString = [NSString stringWithUTF8String:methodTypeEncoding];
     
     IMP dynamicImp;
-    if ([methodTypeEncodingString isEqualToString:@"v@:"]) {
+    if ([self compareMethodCreationTypeEncodings:methodTypeEncodingString :@selector(simpleMethodWithNoParam)]) {
         dynamicImp = [DopamineObject createImpWithNoParam:newSelector :originalSelector];
-    } else if ([methodTypeEncodingString isEqualToString:@"v@:@"]) {
+    } else if ([self compareMethodCreationTypeEncodings:methodTypeEncodingString :@selector(simpleMethodWithObjectParam:)]) {
         dynamicImp = [DopamineObject createImpWithObjectParam:newSelector :originalSelector];
-    } else if ([methodTypeEncodingString isEqualToString:@"v@:i"]) {
+    } else if ([self compareMethodCreationTypeEncodings:methodTypeEncodingString :@selector(simpleMethodWithIntParam:)]) {
         dynamicImp = [DopamineObject createImpWithIntParam:newSelector :originalSelector];
     } else {
         NSLog(@"Unsupported encoding:%@", methodTypeEncodingString);
@@ -41,6 +41,18 @@
     return newSelector;
 }
 
++ (BOOL) compareMethodCreationTypeEncodings :(NSString*) candidate :(SEL) templateSelector {
+    Method templateMethod = class_getInstanceMethod([DopamineObject self], templateSelector);
+    if (templateMethod == nil) {
+        return false;
+    }
+    const char* templateMethodTypeEncoding = method_getTypeEncoding(templateMethod);
+    
+    NSString* templateMethodTypeEncodingString = [NSString stringWithUTF8String:templateMethodTypeEncoding];
+    return [templateMethodTypeEncodingString isEqualToString:candidate];
+}
+
+- (void) simpleMethodWithNoParam { }
 + (IMP) createImpWithNoParam :(SEL) selector :(SEL) originalSelector {
     IMP dynamicImp = imp_implementationWithBlock(^(id self) {
         if (!self || ![self respondsToSelector:selector]) {return;}
@@ -53,6 +65,7 @@
     return dynamicImp;
 }
 
+- (void) simpleMethodWithObjectParam :(id) param1 { }
 + (IMP) createImpWithObjectParam :(SEL) selector :(SEL) originalSelector {
     IMP dynamicImp = imp_implementationWithBlock(^(id self, id object) {
         if (!self || ![self respondsToSelector:selector]) {return;}
@@ -64,6 +77,7 @@
     return dynamicImp;
 }
 
+- (void) simpleMethodWithIntParam :(int) param1 { }
 + (IMP) createImpWithIntParam :(SEL) selector :(SEL) originalSelector {
     IMP dynamicImp = imp_implementationWithBlock(^(id self, int object) {
         if (!self || ![self respondsToSelector:selector]) {return;}
