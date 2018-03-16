27e1d31d9e21046523808814f781faa67c92f86b
Akash Desai <kash650@gmail.com>
2018-03-15 20:13:39 -0700

    added rewards view controller

---
+++ Example/BoundlessKit/Reward.swift
@@ -0,0 +1,40 @@
+//
+//  Reward.swift
+//  BoundlessKit_Example
+//
+//  Created by Akash Desai on 3/15/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import BoundlessKit
+
+enum Reward : String {
+    case shimmy, pulse, vibrate, rotate, glow, sheen, emojisplosion, confetti
+    static let cases:[Reward] = [.shimmy, .pulse, .vibrate, .rotate, .glow, .sheen, .emojisplosion, .confetti]
+    
+    func test(view: UIView) {
+        let completion = {
+            print("Completed showing reward type:\(self.rawValue)")
+        }
+        switch self {
+        case .shimmy:
+            view.showShimmy(completion: completion)
+        case .pulse:
+            view.showPulse(completion: completion)
+        case .vibrate:
+            view.showVibrate(hapticFeedback: true, systemSound: 1009, completion: completion)
+        case .rotate:
+            view.rotate360Degrees(completion: completion)
+        case .glow:
+            view.showGlow(completion: completion)
+        case .sheen:
+            view.clipsToBounds = true
+            view.showSheen(completion: completion)
+        case .emojisplosion:
+            view.superview!.showEmojiSplosion(at: view.center, completion: completion)
+        case .confetti:
+            view.showConfetti(completion: completion)
+        }
+    }
+}
---
+++ Example/BoundlessKit/RewardsViewController.swift
@@ -0,0 +1,79 @@
+//
+//  RewardsViewController.swift
+//  BoundlessKit_Example
+//
+//  Created by Akash Desai on 3/15/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RewardsViewController : UICollectionViewController {
+    
+    fileprivate let reuseIdentifier = "RewardTypeCell"
+    fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    fileprivate let itemsPerRow: CGFloat = 3
+    
+    static var current: RewardsViewController?
+    
+}
+
+extension RewardsViewController {
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Reward.cases.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! RewardTypeCell
+        
+        cell.label.text = Reward.cases[indexPath.row].rawValue
+        cell.layer.cornerRadius = 25
+        cell.layer.borderWidth = 1
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        Reward.cases[indexPath.item].test(
+            view: collectionView.cellForItem(at: indexPath)!
+        )
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        RewardsViewController.current = self
+    }
+}
+
+
+class RewardTypeCell : UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+}
+
+extension RewardsViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+    
+}
